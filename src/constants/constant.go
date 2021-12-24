cbd8e9fbc07c0c7af2d0000ae0484fc902809794
Affan Zafar <affanzafar201@gmail.com>
2021-12-24 13:08:09 +0530

    constants keyword for console commands

---
+++ src/constants/constant.go
@@ -0,0 +1,25 @@
+package main
+
+// Command
+const (
+	// creates parking lot constant
+	createParkingLot = "create_parking_lot"
+
+	// park vehicle constant
+	park = "park"
+
+	// leave slot constant
+	leave = "leave"
+
+	// status constant
+	status = "status"
+
+	// command for registration no for cars with given color
+	registrationNoForCarsWithColors = "registration_numbers_for_cars_with_colous"
+
+	// command for slot no for cars with given color
+	slotNoForCarsWithColor = "slot_numbers_for_cars_with_colour"
+
+	// command for slot number for given resgitration no
+	slotNoForRegistrationNo = "slot_number_for_registration_number"
+)
